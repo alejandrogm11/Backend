3f8bbe0adae6b530ba1d6538847232505e7cbc45
vplooo <alejandrogm11@educastur.es>
2026-04-13 12:30:11 +0200

    Login en proceso

--- Autenticator/src/layouts/LoginLayout.vue
+++ Autenticator/src/layouts/LoginLayout.vue
@@ -1,34 +1,88 @@
 <template>
-<div class="conenedor">
-<q-card class="my-card flex h-screen items-center justify-center">
-  <q-card-section class="items-center text-center">
-    <div class="text-h6">Login para todos</div>
-  </q-card-section>
-  <q-card-section>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  </q-card-section>
-</q-card>
-</div>
+  <div class="contenedor">
+    <q-card class="my-card" bordered>
+      <q-card-section class="items-center text-center">
+        <div class="text-h6">Login para todos</div>
+      </q-card-section>
 
+      <q-separator dark inset/>
+
+      <q-card-section class="text-center">
+        <div class="text-h7">Bienvenido</div>
+        <br>
+        <q-input standout v-model="email" type="email" label="Email" item-aligned>
+          <template v-slot:prepend>
+            <q-icon name="mail" />
+          </template>
+        </q-input>
+        <q-input standout v-model="email" type="password" label="Password" item-aligned>
+          <template v-slot:prepend>
+            <q-icon name="key" />
+          </template>
+        </q-input>
+        <br>
+        <q-btn push color="primary" icon="login" label="Login" />
+      </q-card-section>
+
+      <q-separator spaced inset vertical dark />
+    </q-card>
+  </div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
+import {ref} from 'vue'
 
-
+const email = ref()
 
 </script>
 <style scoped>
 
-.my-card{
-  max-width: 500px;
-  justify-self: center;
-
+.my-card {
+  max-width: 700;
+  width: 570px;
 }
-.contenedor{
+.my-card {
+  background: rgba(170, 169, 169, 0.425);
+  backdrop-filter: blur(12px);
+  border-radius: 16px;
+  padding: 24px;
+  color: white;
+  border: 1px solid rgba(255, 255, 255, 0.1);
+}
+
+
+
+.contenedor {
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height: 50vh;
+  height: 100vh;
+  width: 100%;
+}
+/* fondo */
+
+.contenedor {
+  background: linear-gradient(
+    120deg,
+    #132c36,
+    #234855,
+    #406f83
+  );
+  background-size: 200% 200%;
+  animation: gradientMove 10s ease-in-out infinite;
+  min-height: 100vh;
 }
 
+/* Animación muy suave */
+@keyframes gradientMove {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
 </style>
