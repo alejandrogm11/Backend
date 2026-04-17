9dff8f328877284cce0d5212e20842016a852577
vplooo <alejandrogm11@educastur.es>
2026-04-17 10:33:02 +0200

    agregando panel de administrador

--- Autenticator/src/layouts/HomeLayout.vue
+++ Autenticator/src/layouts/HomeLayout.vue
@@ -1,56 +1,36 @@
-
 <template>
   <q-layout view="hHh lpR fFf">
-
     <q-header elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+        <q-btn dense flat round icon="menu" @click="leftDrawerOpen = !leftDrawerOpen" />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
           Auth System V 1.0
         </q-toolbar-title>
 
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+        <q-btn dense flat round icon="menu" @click="rightDrawerOpen = !rightDrawerOpen" />
       </q-toolbar>
     </q-header>
 
-    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" elevated>
+    <q-drawer v-model="leftDrawerOpen" side="left" elevated overlay>
       <!-- drawer content -->
     </q-drawer>
 
-    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" elevated>
+    <q-drawer v-model="rightDrawerOpen" side="right" elevatedoverlay>
       <!-- drawer content -->
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
-<script>
-import { ref } from 'vue'
-
-export default {
-  setup () {
-    const leftDrawerOpen = ref(false)
-    const rightDrawerOpen = ref(false)
-
-    return {
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      },
-
-      rightDrawerOpen,
-      toggleRightDrawer () {
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-    }
-  }
-}
+<script setup lang="ts">
+import { ref } from 'vue';
+const leftDrawerOpen = ref(false);
+const rightDrawerOpen = ref(false);
 </script>
--- Autenticator/src/pages/AdminDashboard.vue
+++ Autenticator/src/pages/AdminDashboard.vue
@@ -1,3 +1,42 @@
 <template>
-  <h1>ADMIN</h1>
+  <q-page class="q-pa-md">
+    <div class="row q-gutter-md">
+      <div class="col-12 col-md-6 col-lg-3">
+        <q-card>
+          <div class="text-h5 text-weight-bold">Role Manager</div>
+          <q-separator spaced inset vertical dark />
+          <div class="searchBar">
+            <q-input v-model="si" type="search" label="Buacar Usuario" class="">
+              <template v-slot:append>
+                <q-icon name="troubleshoot" />
+              </template>
+            </q-input>
+          </div>
+        </q-card>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-3">
+        <q-card>Stats</q-card>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-3">
+        <q-card>Stats</q-card>
+      </div>
+
+      <div class="col-12 col-md-6 col-lg-3">
+        <q-card>Stats</q-card>
+      </div>
+    </div>
+  </q-page>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const si = ref();
+</script>
+<style scoped lang="scss">
+.searchBar {
+  padding: 10px;
+}
+</style>
